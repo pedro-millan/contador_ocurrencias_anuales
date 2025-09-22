779a9e7b5ea8e3937253c5ae74f1d39f5d881ac2
Pedro Millán <millanpedrop@gmail.com>
2025-09-22 23:47:45 +0200

    Mejoras en introducción de datos y su validación.

--- calendar_exercise.py
+++ calendar_exercise.py
@@ -1,8 +1,8 @@
 import calendar
 
-class ValorIncorrecto(ValueError):
-     def __init__(self, message):
-         super().__init__(message)
+class DiaIncorrecto(ValueError):
+    def __init__(self, message):
+        super().__init__(message)
 
 class MiCalendario(calendar.Calendar):
     """Subclase de calendar.Calendar que cuenta cuántas veces ocurre
@@ -12,25 +12,36 @@ class MiCalendario(calendar.Calendar):
          super().__init__()
 
     def count_weekday_in_year(self, year, weekday):
-        """Devuelve el número de veces que aparece un día de la semana (0=Lunes..6=Domingo)
-            en un año dado. Lanza ValorIncorrecto si el weekday no está en el rango 0–6."""
-            
-        if weekday < 0 or weekday > 6:
-            # Validamos que el día de la semana esté en el rango correcto
-            raise ValorIncorrecto("*El valor debe estar entre 0 y 6*")
+        """Devuelve el número de veces que aparece un día de la semana (Lunes, Domingo, etc.)
+            en un año dado. Lanza ValorIncorrecto si el weekday no es un día de la semana válido."""
         
         contador = 0
         for mes in range(1, 13):
-            # monthdays2calendar devuelve semanas -> listas de tuplas (día, weekday):
             for semana in self.monthdays2calendar(year, mes):
-                #Anidamos bucles que recorrerán cada día de las diferentes semanas
                 for dia, wd in semana:
-                    if dia != 0 and wd == weekday: # descartamos días de relleno (0)
+                    if dia != 0 and wd == weekday:
                         contador += 1
         return contador
+    
+dias_semana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
 
 if __name__ == "__main__":
     year = int(input("Introduce un año: "))
-    weekday = int(input("Introduce un día de la semana en forma de número (donde 0 es Lunes y 6 Domingo): "))
+
+    # Validación del input con while
+    while True:
+            try:
+                dia_semana = input("Introduce el nombre de un día de la semana: ")
+                dia_semana = dia_semana.capitalize(); dia_semana.strip()
+                if dia_semana not in dias_semana:
+                    raise DiaIncorrecto("Error. introduce un día de la semana válido.")
+                weekday = dias_semana.index(dia_semana)
+                break
+            except DiaIncorrecto as e:
+                print(e)
+                
+            
     c = MiCalendario()
-    print(c.count_weekday_in_year(year, weekday))
+    resultado = c.count_weekday_in_year(year, weekday)
+    print(resultado)
+
